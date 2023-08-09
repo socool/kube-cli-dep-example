adfcf21c51714766b98611f5eebd2cbe62403eff
So Cool <127.0.0.1.pm@gmail.com>
2023-08-09 12:15:42 +0700

    initial commit

---
+++ src/main/java/com/k8s/DeployExample/DeployExampleApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.DeployExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8s/DeployExample/DeployExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.DeployExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
